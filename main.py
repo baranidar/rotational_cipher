2e0a5491584b3b0316d89f1168c4bbb8c77b02ed
Vijay Subramanian <47090672+baranidar@users.noreply.github.com>
2024-11-21 22:47:35 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+
+alphabet_dict = {"1":"A","2":"B","3":"C","4":"D","5":"E","6":"F","7":"G","8":"H","9":"I","10":"J","11":"K","12":"L","13":"M","14":"N","15":"O","16":"P","17":"Q","18":"R","19":"S","20":"T","21":"U","22":"V","23":"W","24":"X","25":"Y","0":"Z"}
+
+
+def rotationalCipher(input_str, rotation_factor):
+  orig_list = list(input_str)
+  final_list = []
+  for item in orig_list:
+    if is_alphanumeric(item):
+      final_list.append(get_cipher_value(item,rotation_factor))
+    else:
+      final_list.append(item)
+  return ''.join(final_list)
+  
+  
+def is_alphanumeric(text):
+  return text.isalnum()
+
+def get_cipher_value(input_char, rotation_factor):
+  if input_char.isupper():
+    return alphabet_dict[str((int(list(alphabet_dict.keys())[list(alphabet_dict.values()).index(input_char)])+rotation_factor)%26)]
+  elif input_char.islower():
+    return alphabet_dict[str((int(list(alphabet_dict.keys())[list(alphabet_dict.values()).index(input_char.upper())])+rotation_factor)%26)].lower()
+  else:
+    return str((int(input_char)+rotation_factor)%10)
+
+#-----------------------------------------------------------------------------------------------------------------------------
+# These are the tests we use to determine if the solution is correct.
+# You can add your own at the bottom.
+
+def printString(string):
+  print('[\"', string, '\"]', sep='', end='')
+
+test_case_number = 1
+
+def check(expected, output):
+  global test_case_number
+  result = False
+  if expected == output:
+    result = True
+  rightTick = '\u2713'
+  wrongTick = '\u2717'
+  if result:
+    print(rightTick, 'Test #', test_case_number, sep='')
+  else:
+    print(wrongTick, 'Test #', test_case_number, ': Expected ', sep='', end='')
+    printString(expected)
+    print(' Your output: ', end='')
+    printString(output)
+    print()
+  test_case_number += 1
+
+if __name__ == "__main__":
+  input = "Ahui-#123"
+  rotation_factor = 4
+  output = rotationalCipher(input, rotation_factor)
+  print(output)
